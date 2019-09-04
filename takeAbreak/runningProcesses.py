94db26bbb474078767f4aab21fd5cf0c4cdce9d9
Ibrahim <iam>
2019-09-04 21:38:03 +0200

    fix(runningProcess): Removed enforeced exception; applied linting

--- takeAbreak/runningProcesses.py
+++ takeAbreak/runningProcesses.py
@@ -14,15 +14,14 @@ logging.basicConfig(level=logging.INFO, format='%(process)d-%(levelname)s-%(mess
 try:
     if sys.platform.startswith("win"):
         logging.info("Windows Operating System found..")
-        powershellProcess = subprocess.Popen(["powershell", "Get-Process | select starttime,ProcessName,CPU,PM | Sort "
-                                                            "starttime -Desc"], stdout=subprocess.PIPE)
-        result = powershellProcess.communicate()[0]
-        print(result)
+        POWERSHELL_PROCESS = subprocess.Popen(["powershell", "Get-Process | select starttime,ProcessName,CPU,PM | Sort starttime -Desc"], stdout=subprocess.PIPE)
+        RESULT = POWERSHELL_PROCESS.communicate()[0]
+        print(RESULT)
+
     elif sys.platform.startswith("lin"):
         logging.info("Linux Operating System found..")
-        command = "ps -o pid=,user=,vsz=,command=,%cpu=,%mem,time -e | sort -nrk 3,3 | head -n 7"
-        result = os.system(command)
-        print(10/0)
-except Exception as exception:
-    logging.error("Error ! " , exc_info=True)
+        COMMAND = "ps -o pid=,user=,vsz=,command=,%cpu=,%mem,time -e | sort -nrk 3,3 | head -n 7"
+        RESULT = os.system(COMMAND)
 
+except Exception as exception:
+    logging.error("Error!", exc_info=True)
